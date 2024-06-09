aaf7394b16b159ca708c8e59ed01ab76a1c47717
Ángel Talero <angelgotalero@outlook.com>
2024-06-09 13:23:30 -0500

    Initial application dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    alias(libs.plugins.android)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "dev.taleroangel.telesphoros"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dev.taleroangel.telesphoros"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+
+    /* Koin */
+    implementation(platform(libs.koin.bom))
+    implementation(libs.bundles.koin)
+
+    /* Kotlinx */
+    implementation(libs.bundles.kotlinx)
+
+    /* Jetpack Lifecycle */
+    implementation(libs.bundles.androidx.lifecycle)
+
+    /* Jetpack Compose */
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.androidx.compose)
+    debugImplementation(libs.bundles.androidx.compose.debug)
+
+    /* Jetpack Navigation */
+    implementation(libs.bundles.androidx.navigation)
+
+    /* KSP */
+    ksp(libs.bundles.ksp)
+}
---
+++ app/src/main/kotlin/dev/taleroangel/telesphoros/infrastructure/android/MainApplication.kt
@@ -0,0 +1,5 @@
+package dev.taleroangel.telesphoros.infrastructure.android
+
+import android.app.Application
+
+class MainApplication: Application()
